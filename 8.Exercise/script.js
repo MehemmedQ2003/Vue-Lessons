638d7d36234817b42d6cd1aa1f693a6240b420ae
mehemmed qardasov <116949722+MehemmedQ2003@users.noreply.github.com>
2025-08-29 14:22:00 +0400

    Exercise Vue

---
+++ 8.Exercise/script.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp(
+    {
+        data() {
+            return {
+                name: "Muhammad",
+                age: 22,
+                imageUrl: "https://images.unsplash.com/photo-1756302555654-5e413da2d1b8?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyM3x8fGVufDB8fHx8fA%3D%3D",
+                
+            }
+        }
+    }
+);
+
+app.mount("#frontend");
