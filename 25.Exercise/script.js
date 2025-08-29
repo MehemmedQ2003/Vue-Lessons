96286e5035107b20c2ab175de0161e06fa306148
mehemmed qardasov <116949722+MehemmedQ2003@users.noreply.github.com>
2025-08-29 22:46:35 +0400

    Exercise Vue

---
+++ 25.Exercise/script.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp(
+    {
+        data() {
+            return {
+                inputBackground: "",
+                inputColor: "",
+            }
+        },
+        watch: {
+        },
+        computed: {
+
+        },
+        methods: {
+        },
+    }
+);
+
+app.mount("#frontend");
