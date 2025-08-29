60ed18033cf6369eedb5478b110495057b541644
mehemmed qardasov <116949722+MehemmedQ2003@users.noreply.github.com>
2025-08-29 15:30:06 +0400

    Input Event Vue

--- 11.Input-Event/script.js
+++ 11.Input-Event/script.js
@@ -2,19 +2,12 @@ const app = Vue.createApp(
     {
         data() {
             return {
-                counter: 0,
                 name: "",
             }
         },
         methods: {
             setName(event, lastName) {
                 this.name = event.target.value + " " + lastName;
-            },
-            increase(num) {
-                this.counter += num;
-            },
-            decrease(num) {
-                this.counter -= num;
             }
         }
     }
