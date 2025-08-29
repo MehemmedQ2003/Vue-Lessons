4d99aa4072f0dfb05cf9a8a0c1a1732480351d78
mehemmed qardasov <116949722+MehemmedQ2003@users.noreply.github.com>
2025-08-29 14:43:17 +0400

    Event with Argument Vue

---
+++ 10.Event-With-Argument/script.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp(
+    {
+        data() {
+            return {
+                counter: 0
+            }
+        },
+        methods: {
+            increase(num) {
+                this.counter += num;
+            },
+            decrease(num) {
+                this.counter -= num;
+            }
+        }
+    }
+);
+
+app.mount("#frontend");
