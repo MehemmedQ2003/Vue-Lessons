3d6691ffd11dff7730b0dc332e023813b27507ec
mehemmed qardasov <116949722+MehemmedQ2003@users.noreply.github.com>
2025-08-30 14:46:37 +0400

    V-For Vue

---
+++ 29.V-For/script.js
@@ -0,0 +1,21 @@
+const app = Vue.createApp(
+    {
+        data() {
+            return {
+                skills: [
+                ],
+                skill: "",
+            }
+        },
+        watch: {},
+        computed: {},
+        methods: {
+            addSkill() {
+                this.skills.push(this.skill);
+                this.skill = "";
+            }
+        },
+    }
+);
+
+app.mount("#frontend");
