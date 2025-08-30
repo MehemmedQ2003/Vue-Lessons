2a5057228f2f1efd1eba2beee99bb3b0df31da91
mehemmed qardasov <116949722+MehemmedQ2003@users.noreply.github.com>
2025-08-30 14:28:01 +0400

    Template Vue

---
+++ 26.Template/script.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp(
+    {
+        data() {
+            return {
+            }
+        },
+        watch: {
+        },
+        computed: {
+
+        },
+        methods: {
+        },
+    }
+);
+
+app.mount("#frontend");
